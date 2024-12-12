4dc88b5236a82e5da50db0ef4b1224091631289c
101 <tmddnjs001133@dsm.hs.kr>
2024-12-12 18:52:42 +0900

    Initial commit

---
+++ src/main/kotlin/com/mmm/Application.kt
@@ -0,0 +1,13 @@
+package com.mmm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.reactive.config.EnableWebFlux
+
+@EnableWebFlux
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
