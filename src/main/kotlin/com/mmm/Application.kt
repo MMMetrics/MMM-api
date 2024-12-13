dd143b2058badacadd3e372a10f224dc5ad6d755
101 <tmddnjs001133@dsm.hs.kr>
2024-12-13 23:20:30 +0900

    refactor(configuration): Change Package Structure
    move graphql configuration file to another package

--- src/main/kotlin/com/mmm/Application.kt
+++ src/main/kotlin/com/mmm/Application.kt
@@ -1,10 +1,7 @@
 package com.mmm
 
-import graphql.scalars.ExtendedScalars
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.graphql.execution.RuntimeWiringConfigurer
 import org.springframework.web.reactive.config.EnableWebFlux
 
 @EnableWebFlux
@@ -14,9 +11,3 @@ internal class Application
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }
-
-@Bean
-fun runtimeWiringConfigurer(): RuntimeWiringConfigurer = RuntimeWiringConfigurer {
-    wiringBuilder -> wiringBuilder.scalar(ExtendedScalars.DateTime)
-}
-
