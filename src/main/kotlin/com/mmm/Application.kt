38efc6f1ec73dc043f4fffe8467fe979e3d2d1ae
Seung Won <tmddnjsrla99@gmail.com>
2024-12-17 11:22:27 +0900

    Revert "feat(data): Add TikTok Api Handler and WebClient DSL"

--- src/main/kotlin/com/mmm/Application.kt
+++ src/main/kotlin/com/mmm/Application.kt
@@ -1,13 +1,11 @@
 package com.mmm
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.web.reactive.config.EnableWebFlux
 
 @EnableWebFlux
 @SpringBootApplication
-@ConfigurationPropertiesScan
 internal class Application
 
 fun main(args: Array<String>) {
